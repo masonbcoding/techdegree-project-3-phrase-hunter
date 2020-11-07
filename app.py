fe3a3f436771b8d285859fb70314c4be82dfcfbe
masonbcoding <44744780+masonbcoding@users.noreply.github.com>
2020-11-07 20:57:02 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from phrasehunter.game import Game
+from game import Game
 
+if __name__ == "__main__":
+    game = Game()
 
-if __name__ == '__main__':
-    phraselist = ['Mississippi', 'Schools', 'Should Have', 'Taught', 'Coding']
-    game = Game(phraselist)
-    game.start_game()
+game.start()
+game.game_over()
